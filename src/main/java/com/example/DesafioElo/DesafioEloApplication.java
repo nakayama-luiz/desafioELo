39305102aea65b1f212d57d5cb3a2079b883ae84
nakayama-luiz <luizsantos.naka@gmail.com>
2024-01-20 08:25:04 -0300

    entrada

---
+++ src/main/java/com/example/DesafioElo/DesafioEloApplication.java
@@ -0,0 +1,13 @@
+package com.example.DesafioElo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DesafioElo/DesafioEloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DesafioElo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
